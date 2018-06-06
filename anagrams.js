2345c8722c68442f7aa9ff973409d7d48b914b78
rachd <rachel.m.dorn@gmail.com>
2018-06-06 08:02:07 -0400

    pull in dictionary

--- anagrams.js
+++ anagrams.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 function anagrams(letters, wordSoFar) {
 	if (letters.length === 0) {
 		return [wordSoFar];
@@ -11,4 +13,27 @@ function anagrams(letters, wordSoFar) {
 	return words;
 
 }
-console.log(anagrams(["c", "a", "r", "t", "o", "g", "r", "a", "p", "h", "y"], ""));
+
+function prepareWord(word) {
+	return [...word];
+}
+
+function prepareDictionary() {
+	let data = '';
+
+	const readStream = fs.createReadStream('words_alpha.txt', 'utf8');
+
+	readStream.on('data', function(chunk) {  
+		data += chunk;
+	}).on('end', function() {
+		const dictionary = data.split("\r\n");
+		return (dictionary.slice(0, 20));
+	});
+}
+
+function findAnagrams(word) {
+	const dictionary = prepareDictionary();
+	return anagrams(prepareWord(word), "");
+}
+// console.log(anagrams(["c", "a", "r", "t", "o", "g", "r", "a", "p", "h", "y"], ""));
+console.log(findAnagrams("cat"));
